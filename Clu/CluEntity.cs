c58fb380b6e62049b385068c8fc737f07a5f44d2
Rasheed Ayoade <rashotech@gmail.com>
2024-02-28 01:27:41 +0100

    feat: add clu setup and config

---
+++ Clu/CluEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ChatBot.Clu
+{
+    public class CluEntity
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("length")]
+        public int Length { get; set; }
+
+        [JsonProperty("confidenceScore")]
+        public float ConfidenceScore { get; set; }
+    }
+}
+
