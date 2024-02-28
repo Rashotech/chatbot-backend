490abc970c148203465fd4c3b704d7eb8c025688
Rasheed Ayoade <rashotech@gmail.com>
2024-02-28 01:50:57 +0100

    feat: add cognitive model for bank operation

---
+++ CognitiveModels/BankOperation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder;
+using Newtonsoft.Json;
+
+namespace ChatBot.CognitiveModels
+{
+    public class BankOperation : IRecognizerConvert
+    {
+        public enum Intent
+        {
+            AccountOpening,
+            BranchLocator,
+            LogComplaints,
+            Cancel,
+            BookFlight,
+            CheckingBalance,
+            FundTransfer,
+            GetWeather,
+            TransactionHistory,
+            None
+        }
+
+        public string Text { get; set; }
+
+        public string AlteredText { get; set; }
+
+        public Dictionary<Intent, IntentScore> Intents { get; set; }
+
+        public IDictionary<string, object> Properties { get; set; }
+
+        public void Convert(dynamic result)
+        {
+            var jsonResult = JsonConvert.SerializeObject(result, new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore});
+            var app = JsonConvert.DeserializeObject<BankOperation>(jsonResult);
+
+            Text = app.Text;
+            AlteredText = app.AlteredText;
+            Intents = app.Intents;
+            Properties = app.Properties;
+        }
+
+        public (Intent intent, double score) GetTopIntent()
+        {
+            var maxIntent = Intent.None;
+            var max = 0.0;
+            foreach (var entry in Intents)
+            {
+                if (entry.Value.Score > max)
+                {
+                    maxIntent = entry.Key;
+                    max = entry.Value.Score.Value;
+                }
+            }
+
+            return (maxIntent, max);
+        }
+    }
+}
