56b39ebe6a043bb3544ddb808ffe0445b63c34c9
Rasheed Ayoade <rashotech@gmail.com>
2024-02-28 01:48:52 +0100

    feat: add adaptive card helper

---
+++ Utils/AdaptiveCardHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace ChatBot.Utils
+{
+	public class AdaptiveCardHelper
+	{
+
+        public static Attachment CreateAdaptiveCardAttachment(string cardName)
+        {
+            var filePath = Path.Combine(".", "Resources", $"{cardName}.json");
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
+
