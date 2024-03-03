7afb20d9dac9f78f2e2d6a9d083e61efe3e50465
Rasheed Ayoade <rashotech@gmail.com>
2024-03-03 20:01:23 +0100

    feat: add otp provider

---
+++ Services/Interfaces/INotificationProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChatBot.Services.Interfaces
+{
+	public interface INotificationProvider
+	{
+        Task<string> SendOtpAsync(string phoneNumber);
+        Task<bool> VerifyOtpAsyc(string verificationReference, string verificationCode);
+    }
+}
+
