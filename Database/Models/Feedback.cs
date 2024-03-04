ee9acf98a48681557e3371f9302ce9cc263fa94a
Samuel Okafor <samuelvokafor@gmail.com>
2024-03-05 00:25:01 +0100

    Created a Feedback Model and Migrated to the DB

---
+++ Database/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatBot.Database.Models
+{
+    public class Feedback : BaseEntity
+    {
+        public Feedback() { }
+
+        public int AccountId { get; set; }
+        [ForeignKey("AccountId")]
+        public int Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
---
+++ Repositories/FeedbackRepository.cs
@@ -0,0 +1,36 @@
+﻿using ChatBot.Database;
+using ChatBot.Database.Models;
+using ChatBot.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChatBot.Repositories
+{
+    public class FeedbackRepository : IFeedbackRepository
+    {
+        private readonly BankDbContext _dbContext;
+
+        public FeedbackRepository(BankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Feedback> GetFeedbackByIdAsync(int id)
+        {
+            return await _dbContext.Feedbacks.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Feedback>> GetAllFeedbackAsync()
+        {
+            return await _dbContext.Feedbacks.ToListAsync();
+        }
+
+        public async Task<Feedback> SaveFeedbackAsync(Feedback feedback)
+        {
+            _dbContext.Feedbacks.Add(feedback);
+            await _dbContext.SaveChangesAsync();
+            return feedback;
+        }
+    }
+}
