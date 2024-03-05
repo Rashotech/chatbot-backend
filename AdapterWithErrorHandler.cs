64d68b75b3c6d71d79fc8e8cd83829d362a68179
Rasheed Ayoade <rashotech@gmail.com>
2024-03-05 12:31:12 +0100

    new update

--- AdapterWithErrorHandler.cs
+++ AdapterWithErrorHandler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Bot.Builder.Integration.AspNet.Core;
+﻿using System;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.TraceExtensions;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Logging;
@@ -7,7 +9,7 @@ namespace ChatBot
 {
     public class AdapterWithErrorHandler : CloudAdapter
     {
-        public AdapterWithErrorHandler(BotFrameworkAuthentication auth, ILogger<IBotFrameworkHttpAdapter> logger)
+        public AdapterWithErrorHandler(BotFrameworkAuthentication auth, ILogger<IBotFrameworkHttpAdapter> logger, ConversationState conversationState)
             : base(auth, logger)
         {
             OnTurnError = async (turnContext, exception) =>
@@ -16,8 +18,19 @@ namespace ChatBot
                 logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
 
                 // Send a message to the user
-                await turnContext.SendActivityAsync("The bot encountered an error or bug.");
-                await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.");
+                await turnContext.SendActivityAsync("Please contact FirstContact on 0700-34778-2668228, 01-4485500, 0708-062-5000 for enquiries");
+
+                if (conversationState != null)
+                {
+                    try
+                    {
+                        await conversationState.DeleteAsync(turnContext);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError(e, $"Exception caught on attempting to Delete ConversationState : {e.Message}");
+                    }
+                }
 
                 // Send a trace activity, which will be displayed in the Bot Framework Emulator
                 await turnContext.TraceActivityAsync("OnTurnError Trace", exception.Message, "https://www.botframework.com/schemas/error", "TurnError");
