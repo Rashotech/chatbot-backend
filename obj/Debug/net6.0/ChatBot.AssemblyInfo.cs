cb14a923d49da6e8d9012665b8eb1be06904f7b2
Rasheed Ayoade <rashotech@gmail.com>
2024-03-05 09:24:40 +0100

    feat: add QnA dialog

--- obj/Debug/net6.0/ChatBot.AssemblyInfo.cs
+++ obj/Debug/net6.0/ChatBot.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9acf98a48681557e3371f9302ce9cc263fa94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
